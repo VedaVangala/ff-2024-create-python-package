7577cc796bc6e1a6e9af1059f295f94ec15d492a
Veda Naga Priya Vangala <vedanagapriya@usf.edu>
2024-10-30 16:58:34 +0000

    add_num

---
+++ src/mypackage/add_number.py
@@ -0,0 +1,18 @@
+"""A module that adds numbers together"""
+
+def add_num(num_1, num_2):
+    """Here is a functions that adds two numbers together
+    
+    Parameters
+    ----------
+    
+    num_1 : int
+        The first number that the function will add
+    num_2 : int
+        The second number that the function will add
+        
+    returns
+    ____________
+    the sum of two numbers"""
+    
+    return num_1 + num_2
